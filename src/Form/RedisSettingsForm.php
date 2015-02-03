177fe1c0ae58908cbb672ad6bdef6864d9919148
Sascha Grossenbacher <saschagros@gmail.com>
2015-02-03 15:00:27 +0100

    Fix fatal error in settings form

--- src/Form/RedisSettingsForm.php
+++ src/Form/RedisSettingsForm.php
@@ -106,7 +106,7 @@ class RedisSettingsForm extends ConfigFormBase {
       // Numeric values can be both of NULL or 0 (NULL meaning the value is not
       // not set and the client will use the default, while 0 has a business
       // meaning and should be kept as is).
-      if ('0' !== $form_state->getValue($name) && empty($form_state->getValue($name))) {
+      if ('0' !== $form_state->getValue($name) && !$form_state->getValue($name)) {
         $config->clear('connection.' . $name);
       }
       else {
